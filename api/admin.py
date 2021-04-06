2ddf66a9bfb9796e97e775da21b8aeb7b4960b2f
ussii39 <usimarutetunori3@outlook.jp>
2021-04-06 18:02:48 +0900

    initial commit

---
+++ api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Task, Profile
+
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Profile)
+# Register your models here.
